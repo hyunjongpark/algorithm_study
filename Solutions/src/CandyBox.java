c004fcf55ff914af69d479d6688f70c18e31c59d
HyunJong Park <paulhj.park@samsung.com>
2017-09-22 13:51:52 +0900

    first commit

---
+++ Solutions/src/CandyBox.java
@@ -0,0 +1,116 @@
+/*
+ * https://www.acmicpc.net/problem/2243
+ */
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CandyBox {
+	static int N = 1000000;
+	static int M = 0;
+	static int K = 0;
+	static long[] arr = null;
+	static long[] tree = null;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.setIn(new FileInputStream("candybox"));
+		InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringTokenizer token;
+        
+        
+		int T = Integer.parseInt(br.readLine());
+		int size = (int) Math.pow(2, height() + 1);
+		tree = new long[size];
+		arr = new long[N + 1];
+		for (int i = 1; i <= N; i++) {
+			arr[i] = 0;
+		}
+		init(1, 0, N - 1);
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < T; i++) {
+			token = new StringTokenizer(br.readLine());
+			int type = Integer.parseInt(token.nextToken());
+			if (type == 2) {
+				int changeIndex = Integer.parseInt(token.nextToken()) - 1;
+				int changeValue = Integer.parseInt(token.nextToken());
+				arr[changeIndex] = arr[changeIndex] + changeValue;
+				update(1, 0, N - 1, changeIndex, changeValue);
+			} else {
+				int index = Integer.parseInt(token.nextToken());
+				int r = findIndex(1, 0, N - 1, index, -1);
+				sb.append(r + "\n");
+			}
+		}
+		System.out.print(sb);
+	}
+
+	public static long init(int node, int start, int end) {
+		if (start == end) {
+			tree[node] = arr[start];
+//			System.out.println("node: " + node + " start: " + start + " end: " + end + " tree[node]: " + tree[node]);
+			return arr[start];
+		}
+
+		tree[node] = init(node * 2, start, (start + end) / 2) + init(node * 2 + 1, (start + end) / 2 + 1, end);
+//		System.out.println("node: " + node + " start: " + start + " end: " + end + " tree[node]: " + tree[node]);
+		return tree[node];
+	}
+
+	public static void update(int node, int start, int end, int index, int diff) {
+		if (index < start || end < index) {
+			return;
+		}
+		tree[node] = tree[node] + diff;
+//		System.out.println("node: " + node + " start: " + start + " end: " + end + " index: " + index + " tree[node]: "+ tree[node]);
+		if (start == end) {
+			return;
+		}
+		update(node * 2, start, (start + end) / 2, index, diff);
+		update(node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
+	}
+
+	public static int findIndex(int node, int start, int end, int index, int diff) {
+		int findNode = start;
+//		System.out.println("node: " + node + " start: " + start + " end: " + end + " index: " + index + " tree[node]: "+ tree[node]);
+		if (start == end) {
+			tree[node] = tree[node] -1;
+			return start + 1;
+		}
+		long saa = sum(1, 0, N - 1, 0, (start + end) / 2);
+		if (saa >= index) {
+			findNode = findIndex(node * 2, start, (start + end) / 2, index, diff);
+			tree[node] = tree[node] - 1;
+		} else {
+			findNode = findIndex(node * 2 + 1, (start + end) / 2 + 1, end, index, diff);
+			tree[node] = tree[node] -1;
+		}
+
+		return findNode;
+	}
+
+	public static long sum(int node, int start, int end, int left, int right) {
+		if (end < left || right < start) {
+			return 0;
+		}
+		if (left <= start && end <= right) {
+//			System.out.println("sum node: " + node + " start: " + start + " end: " + end + " tree[node]: " + tree[node]);
+			return tree[node];
+		}
+		return sum(node * 2, start, (start + end) / 2, left, right) + sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+	}
+
+	private static int height() {
+		int i = 1;
+		while (true) {
+			if (Math.pow(2, i) > N) {
+				break;
+			}
+			i++;
+		}
+		return i;
+	}
+}
---
+++ Solutions/src/Segments_10999.java
@@ -0,0 +1,112 @@
+/*
+ * https://www.acmicpc.net/problem/10999
+ */
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Segments_10999 {
+	static int N, M, K;
+	static long arr[];
+	static long lazy[];
+	static long tree[];
+
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("10999"));
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		int height = (int)(Math.ceil(Math.log(N) / Math.log(2)));
+		int maxSize = (int)(Math.pow(2, height + 1) - 1);
+		tree = new long[maxSize + 1];
+		lazy = new long[maxSize + 1];
+		arr = new long[N + 1];
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		init(1, 1, N);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M + K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int type = Integer.parseInt(st.nextToken());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			if (type == 1) {
+				int diff = Integer.parseInt(st.nextToken());
+				updateRange(1, 1, N, diff, start, end);
+			} else {
+				long ret = sum(1, 1, N, start, end);
+				sb.append(ret + "\n");
+			}
+		}
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+	public static long init(int node, int start, int end) {
+		if (start == end) {
+			tree[node] = arr[start];
+//			 System.out.println("node: " + node + " start: " + start + " end: " + end + " tree[node]: " + tree[node]);
+			return arr[start];
+		}
+
+		tree[node] = init(node * 2, start, (start + end) / 2) + init(node * 2 + 1, (start + end) / 2 + 1, end);
+//		 System.out.println("node: " + node + " start: " + start + " end: " + end + " tree[node]: " + tree[node]);
+		return tree[node];
+	}
+	
+	public static void update_lazy(int node, int start, int end) {
+		if (lazy[node] != 0) {
+			tree[node] += (lazy[node] * (end - start + 1));
+			if (start != end) {
+				lazy[node * 2] += lazy[node];
+				lazy[node * 2 + 1] += lazy[node];
+			}
+			lazy[node] = 0;
+		}
+	}
+
+	public static void updateRange(int node, int left, int right, long diff, int start, int end) {
+//		System.out.println("stIdx: " + start + " edIdx: " + end + " node: " + node + " stRange: " + left + " edRange: " + right);
+		update_lazy(node, left, right);
+		if (right < start || end < left) {
+			return;
+		}
+		if (start <= left && right <= end) {
+			tree[node] += (diff * (right - left + 1));
+			if (left != right) {
+				lazy[node * 2] += diff;
+				lazy[node * 2 + 1] += diff;
+			}
+			return;
+		}
+		int mid = (left + right) / 2;
+		updateRange(node * 2, left, mid, diff, start, end);
+		updateRange(node * 2 + 1, mid + 1, right, diff, start, end);
+		tree[node] = tree[node * 2] + tree[node * 2 + 1];
+	}
+
+
+	public static long sum(int node, int start, int end, int left, int right) {
+		update_lazy(node, start, end);
+		if (end < left || right < start) {
+			return 0;
+		}
+		if (left <= start && end <= right) {
+			// System.out.println(" node: " + node + " start: " + start + " end:
+			// " + end + " tree[node]: " + tree[node]);
+			return tree[node];
+		}
+		return sum(node * 2, start, (start + end) / 2, left, right)
+				+ sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+	}
+}
+
